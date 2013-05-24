84bbcc3233bfccfc27a8f9de38ffef3bafd9618e
Matt Stock <stock@bexkat.com>
2013-05-23 23:01:13 -0400

    fit all of the control code, added dirty indicator and external interrupt.

--- cpld/cocofdc.v
+++ cpld/cocofdc.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 
 module cocofdc (c_eclk, c_cts_n, c_scs_n, m_databus, c_databus, c_nmi_n, c_halt_n, c_reset_n, addrbus, c_rw, miso, mosi, sclk, ss,
-					m_cs1_n, m_cs2_n, m_we_n, m_oe_n, c_slenb_n, c_busen_n, clock_50, reset, c_cart_n, led);
+					m_cs1_n, m_cs2_n, m_we_n, m_oe_n, c_slenb_n, c_busen_n, clock_50, reset, led, p_int);
 
 inout [14:0] addrbus; // Shared address bus
 inout [7:0] m_databus; // Memory databus
@@ -11,7 +11,6 @@ input c_cts_n; // 0 = Coco ROM read
 input c_eclk; // 1 = memory half of Coco bus cycle
 output c_slenb_n;
 output c_nmi_n;
-output c_cart_n;
 input c_rw; // Coco read/write
 input c_reset_n; // 0 = Coco reset
 output [3:0] led;
@@ -21,6 +20,7 @@ output m_cs1_n; // SRAM 0x0000-0x7fff chip select
 output m_cs2_n; // SRAM 0x8000-0xffff chip select
 output c_halt_n;
 output c_busen_n;
+output p_int;
 input sclk;
 input clock_50;
 input mosi;
@@ -47,6 +47,7 @@ reg spi_control;
 reg spi_haltreq;
 reg spi_rw;
 reg [2:0] sram_write_cnt;
+reg p_int; // mark when Coco writes to SCS memory
 
 // Basic bus control logic
 assign c_select = ((~c_scs_n & c_eclk) | ~c_cts_n) & c_reset_n;
@@ -56,14 +57,13 @@ assign addrbus = (spi_control ? spi_address[14:0] : 15'bz);
 assign c_databus = (m_we_n & ~c_busen_n ? m_databus : 8'bz);
 assign m_databus = (m_we_n ? 8'bz : (spi_control ? spi_databyte : c_databus));
 
-assign m_cs1_n = ~((spi_control & spi_address[15]) | (c_select & ~c_scs_n));
+assign m_cs1_n = ~((spi_control & spi_address[15]) | (c_select & ~c_cts_n));
 assign m_cs2_n = ~m_cs1_n;
 assign m_oe_n = (spi_control ? ~spi_rw : ~(c_select & c_rw));
 assign m_we_n = (spi_control ? spi_rw : 1'b1);
 
 // Safe defaults
 assign c_slenb_n = 1'bz;
-assign c_cart_n = 1'bz;
 assign c_nmi_n = 1'bz;
 assign c_halt_n = 1'bz;
 
@@ -106,6 +106,8 @@ begin
 		state <= 3'h0;
 	 end
 	 3'h4: begin // clock out read byte
+		if (!spi_address[15]) // Clear the read markers
+		  p_int <= 1'b0;
 	   spi_address <= spi_address + 1'b1;
 	   state <= 3'h0;
 	 end
@@ -123,6 +125,8 @@ always @(negedge reset or posedge clock_50) begin
 	 spi_rw <= 1'b1;
 	 sram_write_cnt <= 3'b0;
   end else begin
+    if (c_select & ~c_scs_n & ~c_rw)
+	   p_int <= 1'b1;
     if (sram_write_cnt == 0)
 	   spi_rw <= 1'b1;
 	 else if (sram_write_cnt == 1) begin
