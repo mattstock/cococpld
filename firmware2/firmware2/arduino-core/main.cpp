8cb3a421b3a8f3d2583162cccea1d2e4ce79d141
Matt Stock <stock@bexkat.com>
2013-10-14 13:57:56 -0400

    moving to makefiles and linux based dev environment

---
+++ firmware2/firmware2/arduino-core/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+int arduinomain(void)
+{
+	init();
+
+#if defined(USBCON)
+	USBDevice.attach();
+#endif
+	
+	setup();
+    
+	for (;;) {
+		loop();
+		if (serialEventRun) serialEventRun();
+	}
+        
+	return 0;
+}
+
